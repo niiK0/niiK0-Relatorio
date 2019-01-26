ee631597c3c1cad04017a8f06908578e3882a2c3
niiK0 <37949374+niiK0@users.noreply.github.com>
2019-01-26 03:14:14 +0000

    Add files via upload

---
+++ niiK0-Relatorio/__resource.lua
@@ -0,0 +1,16 @@
+description 'Made by: niiK0 | Relatorio a staff'
+
+version '1.0.0'
+
+client_script {
+
+  'client.lua'
+
+}
+
+server_scripts {
+
+  '@mysql-async/lib/MySQL.lua',
+  'server.lua'
+
+}
